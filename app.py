90bef2bb38b0364ee32674df438a4e6ede314ebb
Guilherme Simões Lourenço <161139361+devlourenco@users.noreply.github.com>
2024-07-10 19:40:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-#Pegar os dados da planilha
+
 import openpyxl 
 from PIL import Image,ImageDraw, ImageFont
-#Abrindo a planilha
+
 workbook_alunos = openpyxl.load_workbook('planilha_alunos.xlsx')
 sheet_alunos = workbook_alunos['Sheet1']
 
 for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2, max_row=2)):
-    #acessar cada celula que contém a info que precisamos 
-    nome_curso = linha[0].value #nome do curso
-    nome_participante = linha[1].value # nome do participante
-    tipo_participacao = linha[2].value # tipo de participação
-    data_inicio = linha[3].value # data inicio
-    data_final = linha[4].value # data final
-    carga_horaria = linha[5].value # carga horaria
-    data_emissao = linha[6].value # data emissao certificado
+    
+    nome_curso = linha[0].value 
+    nome_participante = linha[1].value 
+    tipo_participacao = linha[2].value 
+    data_inicio = linha[3].value 
+    data_final = linha[4].value 
+    carga_horaria = linha[5].value 
+    data_emissao = linha[6].value 
     input('')
 
-    #Transferis os dados da planilha para o certificado
+   
     fonte_nome = ImageFont.truetype('./tahomabd.ttf', 90)
     fonte_geral = ImageFont.truetype('./tahoma.ttf', 80)
     fonte_data = ImageFont.truetype('./tahoma.ttf', 70)
